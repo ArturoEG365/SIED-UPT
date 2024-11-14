854f9377f027f117adad62499102451d4aa7c6fd
Arturo Elizalde <arturoelizalde@Arturo--MacBook-Air.local>
2024-11-14 13:07:19 -0600

    Add concurrencly in service BoardOfDirector

--- src/main/java/com/sied/clients/repository/boardOfDirector/BoardOfDirectorRepository.java
+++ src/main/java/com/sied/clients/repository/boardOfDirector/BoardOfDirectorRepository.java
@@ -4,6 +4,11 @@ import com.sied.clients.entity.boardOfDirector.BoardOfDirector;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repositorio para la entidad BoardOfDirector.
+ * Proporciona métodos CRUD básicos para interactuar con la base de datos a través de JpaRepository.
+ * Este repositorio hereda las funcionalidades estándar de JpaRepository, como guardar, buscar, actualizar y eliminar registros.
+ */
 @Repository
 public interface BoardOfDirectorRepository extends JpaRepository<BoardOfDirector, Long> {
 }
